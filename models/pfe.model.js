3d63244e6bffe669666e99055d6a39972ce6880e
Bejaoui Aymen <aymen.bejaoui1997@gmail.com>
2021-06-11 01:56:51 +0200

    First commit

---
+++ models/pfe.model.js
@@ -0,0 +1,23 @@
+mongoose = require('mongoose') ;
+var Schema = mongoose.Schema;
+
+var Schema = new mongoose.Schema ({
+   
+    pfe : {
+        type : String
+    },
+    added_by_id : {
+        type : String
+    },
+    added_by_name : {
+        type : String
+    },
+    accepted_by_id : {
+        type : String
+    },
+    accepted_by_name : {
+        type : String
+    }
+}) ;
+
+module.exports = mongoose.model('pfe' , Schema) ;
